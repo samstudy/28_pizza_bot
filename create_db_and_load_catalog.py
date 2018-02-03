f4b51e86dbeae4fb96053e3a70ad95f32c39ee15
samstudy <samat.study@gmail.com>
2018-02-03 11:58:05 +0600

    Added 2 function for able to handle json file

--- create_db_and_load_catalog.py
+++ create_db_and_load_catalog.py
@@ -1,8 +1,23 @@
 from models import Base, engine, session, Pizza, Option
-from meals_catalog import meals
+import json
+import argparse
+import os
 
 
-def insert_into_db(catalog):
+def get_meals_catalog_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('meals_catalog_path', help='Mandatory parametr')
+    return parser.parse_args()
+
+
+def load_meals_catalog_from_file(meals_catalog_path):
+    if not os.path.exists(meals_catalog_path):
+        return None
+    with open(meals_catalog_path, 'r') as meals_catalog:
+        return json.loads(meals_catalog.read())
+
+
+def insert_into_db(meals):
     for meal in meals:
         pizza = Pizza(title=meal['title'], description=meal['description'])
         session.add(pizza)
@@ -15,4 +30,6 @@ def insert_into_db(catalog):
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    meals_catalog = get_meals_catalog_path()
+    meals = load_meals_catalog_from_file(meals_catalog.meals_catalog_path)
     insert_into_db(meals)
